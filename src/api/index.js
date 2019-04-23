7c5e4c3428fd8f1117ef1c40fdeddd619848654b
xzh <xzh19971005@163.com>
2019-04-23 15:50:29 +0800

    commit

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+/**
+ * long description for the file
+ *
+ * @summary short description for the file
+ * @author xzh xzh19971005@163.com
+ *
+ * Created at     : 2019-04-23 15:48:22 
+ * Last modified  : 2019-04-23 15:48:54
+ */
+
