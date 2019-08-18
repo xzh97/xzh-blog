f80abdf18146acbb2a490acd17308be265a8dcc4
xzh <995194437@qq.com>
2019-08-18 23:27:37 +0800

    add component pagination-v2

---
+++ src/components/base/pagination-v2/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pagination-wrapper">
+        这里是首页的pagination
+    </div>
+</template>
+
+<script>
+export default {
+    name:'pagination-v2'
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
