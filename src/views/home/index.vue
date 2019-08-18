33b4b996dd7b757fb2696cd4d658dff1ae49be1c
xzh <995194437@qq.com>
2019-08-18 23:25:16 +0800

    commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,8 +3,12 @@
         <div class="logo">
             <h1 class="logo-text">将你我连同人间浸没</h1>
             <p class="description">且随疾风前行</p>
+            <div class="search-anything">
+                <input class="search-area" type="text" placeholder="Search anything here~">
+            </div>
             <hr>
         </div>
+        
         <ul class="blog-list">
             <h4>记事</h4>
             <li class="blog-item" v-for='blog in blogList' :key='blog.blogOid'>
@@ -13,6 +17,14 @@
                     <p class="title">{{blog.title}}</p>
                     <p class="blog-description">{{blog.description}}</p>
                 </div>
+            </li>
+        </ul>
+
+        <ul class="category-list">
+            <h4>(ฅ´ω`ฅ)</h4>
+            <li class="category-item" :key='category.categoryOid' v-for="(category,index) in categoryList">
+                <span class="category-name">ฅ{{category.name}}ฅ</span>
+                <span v-if="index !== categoryList.length - 1"  class="dot">&nbsp;&nbsp;·&nbsp;&nbsp;</span>
             </li>
         </ul>
     </div>
@@ -46,6 +58,9 @@ export default {
             this.hasNextPage = res.hasNextPage;
             this.hasPrevPage = res.hasPrevPage;
         })
+        getCategories().then(res => {
+            this.categoryList = res;
+        })
     }
 }
 </script>
@@ -64,15 +79,34 @@ export default {
             font-size: 18px;
             margin: 0 0 15px;
         }
-
+        .search-anything {
+            margin: 30px;
+            .search-area {
+                height: 35px;
+                line-height: 1.5;
+                color: $text-color;
+                border-radius: $border-radius-base;
+                border: 2px solid #dddddd;
+                background: rgba(254, 252, 250, 0.6);
+                padding: 6px 10px;
+                font-size: 14px;
+            }
+            .search-area:hover {
+                border-color:#cccccc; 
+            }
+            .search-area-active {
+                border-color: #bbbbbb;
+            }
+        }
+        h4{
+            font-weight: 600;
+            font-size: 20px;
+            line-height: 1.1;
+            color: #34495e;
+        }
         .blog-list {
             width: 100%;
-            h4{
-                font-weight: 600;
-                font-size: 20px;
-                line-height: 1.1;
-                color: #34495e;
-            }
+            
             .blog-item {
                 @include fbc;
                 align-items: baseline;
@@ -81,14 +115,14 @@ export default {
                 line-height: 1.7;
                 margin-bottom: 30px;
                 .blog-item-left {
-                    width: 12%; 
+                    width: 14%; 
                     text-align: right;
                 }
                 .create-time{
                     color:#34495e;
                 }
                 .blog-item-right {
-                    width: 86%; 
+                    width: 84%; 
                     text-align: left;
                     cursor: pointer;
                     .title{
@@ -97,6 +131,26 @@ export default {
                     }
                 }
                 .blog-item-right:hover {
+                    color: #34495e;
+                }
+            }
+        }
+        .category-list {
+            .category-item {
+                display: inline-block;
+                height: 30px;
+                line-height: 1.1;
+                .category-name {
+                    color:$title-color;
+                    cursor: pointer;
+                    font-size: 16px;
+                    transition: all .2s;
+                }
+                .category-name:hover {
+                    color: #34495e;
+                    transition: all .2s;
+                }
+                .dot {
                     color: #34495e;
                 }
             }
