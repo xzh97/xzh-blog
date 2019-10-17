063cb810ecb2c8e7bcaeddcd4cf997104b358d27
xzh <xzh19971005@163.com>
2019-10-17 11:52:23 +0800

    perf(remove no used files): 删除无用文件
    删除views/blog/create文件目录， 删除components/business/category目录，

--- src/main.js
+++ src/main.js
@@ -6,23 +6,6 @@ import router from './routes/index';
 //import './assets/iconfont/iconfont.css'; //暂时先用在线css
 import './styles/animate.css';
 import './styles/common.scss';
-/* import {Button, Select, Switch, Modal, Upload, Icon, Radio, Comment, Avatar, Row, Col,} from 'ant-design-vue';
-const RadioGroup = Radio.Group;
-const Option = Select.Option; */
-
-/* Vue.component(Button.name, Button);
-Vue.component(Select.name, Select);
-Vue.component(Radio.name, Radio);
-Vue.component(RadioGroup.name, RadioGroup);
-Vue.component(Option.name, Option);
-Vue.component(Switch.name, Switch);
-Vue.component(Modal.name, Modal);
-Vue.component(Upload.name, Upload);
-Vue.component(Icon.name, Icon);
-Vue.component(Comment.name, Comment);
-Vue.component(Avatar.name, Avatar);
-Vue.component(Row.name, Row);
-Vue.component(Col.name, Col); */
 
 import directives from './share/directive';
 import message from './plugins/message/index';
--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -1,6 +1,5 @@
 const blogsList = () => import('@/views/blogs/list/index');
 const blogsDetail = () => import('@/views/blogs/detail/index');
-const addBlog = () => import('@/views/blogs/create/index');
 const routes = [
     {
         path:'/blog/list/:categoryOid',
@@ -12,18 +11,6 @@ const routes = [
         path:'/blog/detail/:blogOid',
         name: 'blogDetail',
         component:blogsDetail,
-        meta:{},
-    },
-    {
-        path:'/blog/new',
-        name: 'addNewBlog',
-        component:addBlog,
-        meta:{},
-    },
-    {
-        path:'/blog/update/:blogOid',
-        name: 'updateNewBlog',
-        component:addBlog,
         meta:{},
     },
 ]
