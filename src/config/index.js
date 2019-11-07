01a3b2c1fdcdeaa6cbb10f13a417ad4fdad4ea15
xzh <xzh19971005@163.com>
2019-11-07 15:43:45 +0800

    format:格式化处理以及环境配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,9 @@
-let currentEnvironment = 'dev'
+let currentEnvironment = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
 const environment = {
     "dev": 'http://127.0.0.1:3000',
     'prod': 'http://122.51.73.210:3000'
 };
 const config = {
-    appEnv: 'develop',
     apiUrl: environment[currentEnvironment],
 };
 
--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,40 @@
 /* eslint-disable */
 var path = require('path')
 function resolve (dir) {
-  console.log(__dirname)
-  return path.join(__dirname, dir)
+    console.log(__dirname)
+    return path.join(__dirname, dir)
 }
 module.exports = {
-  lintOnSave: true,
-  css: {
-    loaderOptions: {
-      // 给 sass-loader 传递选项
-      sass: {
-        // @/ 是 src/ 的别名
-        // 所以这里假设你有 `src/variables.scss` 这个文件
-        data: `@import "~@/styles/variables.scss";`
-      },
-      less: {
-          javascriptEnabled:true
-      }
-    }
-  },
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
-  },
-  devServer: {
-      host: 'localhost',
-      port: 8080,
-      //open: true, //配置自动启动浏览器
-      proxy: {
-          '/api': {
-              target: 'localhost:3000/', //对应自己的接口
-              changeOrigin: true,
-              ws: true,
-              pathRewrite: {
+    lintOnSave: true,
+    css: {
+        loaderOptions: {
+            // 给 sass-loader 传递选项
+            sass: {
+            // @/ 是 src/ 的别名
+            // 所以这里假设你有 `src/variables.scss` 这个文件
+            data: `@import "~@/styles/variables.scss";`
+            },
+            less: {
+              javascriptEnabled:true
+            }
+        }
+    },
+    chainWebpack: config => {
+        config.resolve.alias.set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+    },
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        //open: true, //配置自动启动浏览器
+        proxy: {
+            '/api': {
+                target: 'localhost:3000/', //对应自己的接口
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
                   '^/api': ''
-              }
-          }
-      }
-  },
+                }
+            }
+        }
+    }
 }
