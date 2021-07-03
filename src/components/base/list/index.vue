570b0d6f5ddc61767738c138a223a108cbfa93da
xzh <995194437@qq.com>
2021-07-03 10:32:13 +0800

    add list comp

---
+++ src/components/base/list/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list-wrapper">
+        <ul class="custom-list-wrapper">
+            <li slot="renderItem"></li>
+        </ul>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'list',
+    props:{
+        dataSource: {
+            type: Array,
+            default(){
+                return []
+            }
+        },
+        listKey: {
+            type: String,
+        }
+
+    },
+    setup(props){
+
+    }
+}
+</script>
+
+<style lang='scss'>
+.custom-list-wrapper{
+    padding: .3rem;
+}
+</style>
